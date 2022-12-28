84059e085b177db66627882cd6d0c64fbeda78a0
KSorte <71705332+KSorte@users.noreply.github.com>
2022-12-28 00:53:41 -0500

    Update and rename Math - Plus One to Math-LeetCode66-PlusOne.cpp

--- Math-LeetCode66-PlusOne.cpp
+++ Math-LeetCode66-PlusOne.cpp
@@ -1,8 +1,7 @@
 /*You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
 
 Increment the large integer by one and return the resulting array of digits.*/
-class Solution {
-public:
+
     vector<int> plusOne(vector<int>& digits) {
 
     int flag = 0;
@@ -16,4 +15,5 @@ public:
     if (flag == 0){digits.insert(digits.begin(),1); }
     return digits;
     }
-};
+
+
