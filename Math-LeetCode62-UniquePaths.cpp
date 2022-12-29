5e0214c8fb972f1b85cb97004116463776a692b3
KSorte <71705332+KSorte@users.noreply.github.com>
2022-12-29 11:04:56 -0500

    Create Math-LeetCode62-UniquePaths.cpp

---
+++ Math-LeetCode62-UniquePaths.cpp
@@ -0,0 +1,23 @@
+/*There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+
+Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+The test cases are generated so that the answer will be less than or equal to 2 * 109.*/
+#include<iostream>
+using namespace std;
+
+ int uniquePaths(int m, int n) {
+        double product = 1.0;
+        double m1 = (double)m-1;
+        double n1 = (double)n-1;
+        for(int i=1; i<=n-1; i++)
+        {
+            product = product*(m1+i)/i;
+        }
+        return product;
+    }
+int main() {
+   int product = uniquePaths(3,7);
+   cout<<product;
+    return product;
+}
