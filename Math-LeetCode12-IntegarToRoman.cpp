664cf2dd2739bc7159b10ecda51e51ea2d2ac530
KSorte <71705332+KSorte@users.noreply.github.com>
2022-12-29 15:05:29 -0500

    Update Math-LeetCode12-IntegarToRoman.cpp

--- Math-LeetCode12-IntegarToRoman.cpp
+++ Math-LeetCode12-IntegarToRoman.cpp
@@ -68,3 +68,38 @@ int main()
 }
 
 // Output : MMDLXVII
+
+// OR A BETTER WAY
+#include<iostream>
+#include<vector>
+#include<string>
+#include<cmath>
+class Solution {
+public:
+    string intToRoman(int num) {
+        int n[] = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+        string r[] = {"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"};
+
+        string ans;
+        int i = 12;
+        int div;
+        while(num>0)
+        {
+            div = num/n[i];
+            num = num % n[i];
+            while(div--) ans += r[i];
+            i--;
+        }
+
+        return ans;
+    }
+};
+
+int main()
+{ Solution s;
+ int number = 27;
+ string roman;
+ roman = s.intToRoman(number);
+ cout<<roman<<endl;
+ return 0;
+}
