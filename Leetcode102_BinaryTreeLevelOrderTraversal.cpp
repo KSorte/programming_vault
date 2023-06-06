babad851b2ff3390f516c17378e983bdd8233281
Kaushal Sorte <71705332+KSorte@users.noreply.github.com>
2023-06-06 00:01:10 -0400

    Create Leetcode102_BinaryTreeLevelOrderTraversal.cpp

---
+++ Leetcode102_BinaryTreeLevelOrderTraversal.cpp
@@ -0,0 +1,37 @@
+/**
+Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+ # include<queue>
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+    queue<TreeNode*> q;  // Queue for BFS traversal.
+    vector<vector<int>> result; // To be returned
+    if(!root) return result;
+    q.push(root);   // putting root inside the queue.   
+    while (q.size()>0)
+    {
+    vector<int> level;   // To store elements of each level.
+    int length_of_queue = q.size();
+    TreeNode* first_element_queue = new TreeNode();
+    for (int i = 0; i<length_of_queue;i++)
+    {
+        first_element_queue = q.front();
+        q.pop();
+        level.push_back(first_element_queue->val);
+        if(first_element_queue->left)q.push(first_element_queue->left);
+        if(first_element_queue->right)q.push(first_element_queue->right); }
+       
+    if (level.size()>0){result.push_back(level);}
+    }
+    return result; 
+}};
