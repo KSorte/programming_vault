98238f1f7d1f15685c18cec4ab4babf35056d0b7
Kaushal Sorte <kaushalsorte08@gmail.com>
2024-08-08 12:15:09 -0400

    Randomize matrix, cin sizes, computation time

--- miscellaneous/convolution.cpp
+++ miscellaneous/convolution.cpp
@@ -1,3 +1,5 @@
+#include <chrono>
+#include <ctime>
 #include <iostream>
 #include <vector>
 
@@ -7,13 +9,13 @@ Create character matrix. First let it be a constant. To help debug.
 (Later turn it into a randomized matrix.)
 
 Initialize filter K as a vector of ints. (You may not need this, we can leverage K in logic).
+
+unsigned integer, negative value wrap around.
 */
 
 struct ConvolutionResult {
     std::vector<std::vector<unsigned char>> Dx;
     std::vector<std::vector<unsigned char>> Dy;
-    int computation_time_Dx;
-    int computation_time_Dy;
     int max_Dx;
     int min_Dx;
     int max_Dy;
@@ -25,7 +27,6 @@ struct ConvolutionResult {
           Dy(rows, std::vector<unsigned char>(columns, 0)) {
             max_Dx = 0;
             max_Dy = 0;
-
             min_Dx = 255;
             min_Dy = 255;
     };
@@ -33,17 +34,22 @@ struct ConvolutionResult {
 
 class Convolution {
 public:
-    Convolution() {
-        // TODO(KSorte):
-        // Create a random matrix M here. Currently constant.
-        // Take number of rows and columns too from cin and initialize.
-        rows = 3;
-        columns = 3;
-        matrix_to_convolve = {
-                {1, 2, 3},
-                {4, 5, 6},
-                {7, 8, 9}
-        };
+    Convolution(int num_rows, int num_columns) :
+        rows(num_rows),
+        columns(num_columns)
+    {
+        matrix_to_convolve.resize(rows);
+        for (auto & row: matrix_to_convolve) {
+            row.resize(columns, 0);
+        }
+
+        // Seed the random number generator with the current time
+        std::srand(static_cast<unsigned int>(std::time(0)));
+        for (auto & row : matrix_to_convolve) {
+            for (auto & element : row) {
+                element = static_cast<unsigned char>(std::rand() % 256);
+            }
+        }
 
         result = ConvolutionResult(rows, columns);
     }
@@ -108,18 +114,43 @@ public:
     }
 
     ConvolutionResult result;
-private:
-
     std::vector<std::vector<int>> matrix_to_convolve;
+private:
     int rows;
     int columns;
 };
 
 int main() {
-    Convolution convolve;
 
+    std::cout<<"Enter the number of rows for the matrix to convolve."<<'\n';
+    int number_of_rows;
+    std::cin>>number_of_rows;
+
+    std::cout<<"Enter the number of columns for the matrix to convolve."<<'\n';
+    int number_of_cols;
+    std::cin>>number_of_cols;
+
+    Convolution convolve = Convolution(number_of_rows, number_of_cols);
+
+    auto start_row_convolve = std::chrono::high_resolution_clock::now();
     convolve.convolve_rows_with_constant_filter_K();
+    auto end_row_convolve = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration_x = end_row_convolve - start_row_convolve;
+    std::cout << "Row-wise convolution time: " << duration_x.count() << " seconds" << '\n';
+
+    auto start_col_convolve = std::chrono::high_resolution_clock::now();
     convolve.convolve_columns_with_constant_filter_K();
+    auto end_col_convolve = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration_y = end_col_convolve - start_col_convolve;
+    std::cout << "Column-wise convolution time: " << duration_y.count() << " seconds" << std::endl;
+
+    std::cout<<"original matrix "<<'\n';
+    for (const auto& row : convolve.matrix_to_convolve) {
+        for (int element : row) {
+            std::cout << element << " ";
+        }
+        std::cout << std::endl;
+    }
 
     std::cout<<"Dx matrix "<<'\n';
     for (const auto& row : convolve.result.Dx) {
