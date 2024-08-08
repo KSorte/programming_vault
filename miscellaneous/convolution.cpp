18e8f1177e415f1bf997046c7559aba651c4c510
Kaushal Sorte <kaushalsorte08@gmail.com>
2024-08-08 15:53:52 -0400

    Add comments and notes

--- miscellaneous/convolution.cpp
+++ miscellaneous/convolution.cpp
@@ -1,34 +1,75 @@
 #include <chrono>
-#include <ctime>
 #include <iostream>
+#include <random>
 #include <vector>
 
 /*
+Author: Kaushal Atul Sorte
+Email: sorte.K@northeastern.edu
+Compile with:
+g++ -std=c++17 -O2 -Wall -Wextra -o convolution /filepath/convolution.cpp
 
-Create character matrix. First let it be a constant. To help debug.
-(Later turn it into a randomized matrix.)
+Assumptions, Considerations and Design Decisions:
+1. Convolution mathematically results into a matrix larger than the original.
+Here, Dx and Dy are considered to be of the same size.
+Thus, while computation on the boundaries, when the algorithm encounters invalid indices, those values are simply not
+considered in the computation.
 
-Initialize filter K as a vector of ints. (You may not need this, we can leverage K in logic).
+Since this is a convolution, the filter is flipped around (as opposed to correlation).
 
-unsigned integer, negative value wrap around.
+2. A Convolution Result struct is written to package all the deliverables into an object.
+
+3. Since the filter K is a constant, it is not taken as an input, rather the logic implicitly
+implements convolution by filter K = [-1, 0, 1].
+This implementation is not designed for expansion or modularity.
+
+4. Unsigned integer is used everywhere as per the question requirement.
+This means that negative values wrap around to 255 and downwards from there.
+
+5. std::mt19937(since C++11) is used for randomly populating a matrix.
+This is chosen for its speed and is based on the Mersenne twister algorithm.
+This article is taken as a reference:
+https://medium.com/@odarbelaeze/how-competitive-are-c-standard-random-number-generators-f3de98d973f0#:~:text=As%20described%20in%20the%20stack,this%20version%20of%20the%20standard
+
+6. C++ Chrono class is used for calculating compute times for row wise and column wise computations.
+
+7. Display of matrices is optional.
 */
 
+// Struct for packaging the convolution result.
 struct ConvolutionResult {
+    // Row wise convolution matrix.
     std::vector<std::vector<unsigned char>> Dx;
+
+    // Column wise convolution matrix.
     std::vector<std::vector<unsigned char>> Dy;
+
+    // Maimum and minimum values in convolution matrices.
     int max_Dx;
     int min_Dx;
     int max_Dy;
     int min_Dy;
 
+    /**
+     * @brief  Implicit default constructor.
+     */
     ConvolutionResult () {}
 
-    ConvolutionResult(int rows, int columns) : Dx(rows, std::vector<unsigned char>(columns, 0)),
-          Dy(rows, std::vector<unsigned char>(columns, 0)) {
-            max_Dx = 0;
-            max_Dy = 0;
-            min_Dx = 255;
-            min_Dy = 255;
+    /**
+     * @brief Constructor to resize Dx and Dy and initializing max-min values in
+     * Dx and Dy.
+     * @param rows Int number of rows.
+     * @param columns Int number of columns.
+     */
+    ConvolutionResult(int rows, int columns) :
+    Dx(rows, std::vector<unsigned char>(columns, 0)),
+    Dy(rows, std::vector<unsigned char>(columns, 0)) {
+
+        // Set default maximum and minimum values.
+        max_Dx = std::numeric_limits<unsigned char>::min();
+        max_Dy = std::numeric_limits<unsigned char>::min();
+        min_Dx = std::numeric_limits<unsigned char>::max();
+        min_Dy = std::numeric_limits<unsigned char>::max();
     };
 };
 
@@ -38,38 +79,40 @@ public:
         rows(num_rows),
         columns(num_columns)
     {
+        // Resize matrix to convolve.
         matrix_to_convolve.resize(rows);
         for (auto & row: matrix_to_convolve) {
             row.resize(columns, 0);
         }
 
-        // Seed the random number generator with the current time
-        std::srand(static_cast<unsigned int>(std::time(0)));
-        for (auto & row : matrix_to_convolve) {
-            for (auto & element : row) {
-                element = static_cast<unsigned char>(std::rand() % 256);
-            }
-        }
+        randomly_populate_matrix();
 
+        // Initialize result object.
         result = ConvolutionResult(rows, columns);
     }
 
-    ~Convolution() {
+    /**
+     * @brief Destructor.
+     */
+    ~Convolution() {}
 
-    }
-
+    /**
+     * @brief Convolve matrix along rows using constant filter K [-1, 0, 1].
+     */
     void convolve_rows_with_constant_filter_K() {
-        unsigned char max_Dx, min_Dx;
         for (int i = 0; i < rows; i ++) {
             for (int j = 0; j < columns; j ++) {
-
-                // Handling row boundaries.
-                // Convolution with constant Filter K implicit in this implementation.
+                // Convolve row-wise with constant filter K = [-1, 0, 1].
+                // Convolution with K handled implicitly in the logic.
                 if (j - 1 >= 0) {
+                    // if the pixel in previous column within matrix limits.
+                    // Ignore if not within bounds.
                     result.Dx[i][j] += matrix_to_convolve[i][j-1];
                 }
 
                 if (j + 1 < columns) {
+                // if the pixel in next column within matrix limits.
+                // Ignore if not within bounds.
                     result.Dx[i][j] -= matrix_to_convolve[i][j+1];
                 }
 
@@ -86,17 +129,23 @@ public:
         }
     }
 
+    /**
+     * @brief Convolve matrix along columns using constant filter K [-1, 0, 1].
+     */
     void convolve_columns_with_constant_filter_K() {
         for (int i = 0; i < rows; i ++) {
             for (int j = 0; j < columns; j ++) {
-
-                // Handling column boundaries.
-                // Convolution with constant Filter K implicit in this implementation.
+                // Convolve column-wise with constant filter K = [-1, 0, 1].
+                // Convolution with K handled implicitly in the logic.
                 if (i - 1 >= 0) {
+                // if the pixel in previous row within matrix limits.
+                // Ignore if not within bounds.
                     result.Dy[i][j] += matrix_to_convolve[i - 1][j];
                 }
 
                 if (i + 1 < rows) {
+                // if the pixel in next row within matrix limits.
+                // Ignore if not within bounds.
                     result.Dy[i][j] -= matrix_to_convolve[i + 1][j];
                 }
 
@@ -113,65 +162,108 @@ public:
         }
     }
 
+    // Convolution result object.
     ConvolutionResult result;
+
+    // Matrix to convolve.
     std::vector<std::vector<int>> matrix_to_convolve;
+
 private:
+    /**
+     * @brief Randomly populate matrix.
+     */
+    void randomly_populate_matrix() {
+        // Obtain a random number from hardware
+        std::random_device rd;
+
+        // Seed the 64-bit Mersenne Twister engine
+        std::mt19937_64 engine(rd());
+
+        // Set limits of unsigned char datatype as distribution limits.
+        std::uniform_int_distribution<unsigned char> dist(
+            std::numeric_limits<unsigned char>::min(),
+            std::numeric_limits<unsigned char>::max());
+
+        // Randomly populate matrix.
+        for (auto & row : matrix_to_convolve) {
+            for (auto & element : row) {
+                element = dist(engine);
+            }
+        }
+    }
+
+    // Rows and colummns received from command line.
     int rows;
     int columns;
 };
 
 int main() {
 
-    std::cout<<"Enter the number of rows for the matrix to convolve."<<'\n';
+    // Get rows and columns from the command line.
+    std::cout<<"Enter rows."<<'\n';
     int number_of_rows;
     std::cin>>number_of_rows;
 
-    std::cout<<"Enter the number of columns for the matrix to convolve."<<'\n';
+    std::cout<<"Enter columns."<<'\n';
     int number_of_cols;
     std::cin>>number_of_cols;
 
+    // Initialize convolution object.
     Convolution convolve = Convolution(number_of_rows, number_of_cols);
 
+    // Row convolution and its computation time.
     auto start_row_convolve = std::chrono::high_resolution_clock::now();
     convolve.convolve_rows_with_constant_filter_K();
     auto end_row_convolve = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> duration_x = end_row_convolve - start_row_convolve;
     std::cout << "Row-wise convolution time: " << duration_x.count() << " seconds" << '\n';
 
+    // Column convolutio and its computation time.
     auto start_col_convolve = std::chrono::high_resolution_clock::now();
     convolve.convolve_columns_with_constant_filter_K();
     auto end_col_convolve = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> duration_y = end_col_convolve - start_col_convolve;
     std::cout << "Column-wise convolution time: " << duration_y.count() << " seconds" << std::endl;
 
-    std::cout<<"original matrix "<<'\n';
-    for (const auto& row : convolve.matrix_to_convolve) {
-        for (int element : row) {
-            std::cout << element << " ";
-        }
-        std::cout << std::endl;
-    }
-
-    std::cout<<"Dx matrix "<<'\n';
-    for (const auto& row : convolve.result.Dx) {
-        for (int element : row) {
-            std::cout << element << " ";
-        }
-        std::cout << std::endl;
-    }
-
+    // Print maximum and minimum in Dx and Dy.
+    std::cout<<'\n';
     std::cout<<"Dx matrix max = "<<convolve.result.max_Dx<<'\n';
     std::cout<<"Dx matrix min = "<<convolve.result.min_Dx<<'\n';
-
-    std::cout<<"Dy matrix "<<'\n';
-    for (const auto& row : convolve.result.Dy) {
-        for (int element : row) {
-            std::cout << element << " ";
-        }
-        std::cout << std::endl;
-    }
-
     std::cout<<"Dy matrix max = "<<convolve.result.max_Dy<<'\n';
     std::cout<<"Dy matrix min = "<<convolve.result.min_Dy<<'\n';
 
+    std::cout<<"Diplay matrices? (y/n)"<<'\n';
+
+    std::string display_matrices;
+    std::cin>>display_matrices;
+
+    if (display_matrices == "y") {
+        // Print all three matrices.
+        std::cout<<'\n';
+        std::cout<<"original matrix "<<'\n';
+        for (const auto& row : convolve.matrix_to_convolve) {
+            for (int element : row) {
+                std::cout << element << " ";
+            }
+            std::cout << std::endl;
+        }
+
+        std::cout<<'\n';
+        std::cout<<"Dx matrix "<<'\n';
+        for (const auto& row : convolve.result.Dx) {
+            for (int element : row) {
+                std::cout << element << " ";
+            }
+            std::cout << std::endl;
+        }
+
+        std::cout<<'\n';
+        std::cout<<"Dy matrix "<<'\n';
+        for (const auto& row : convolve.result.Dy) {
+            for (int element : row) {
+                std::cout << element << " ";
+            }
+            std::cout << std::endl;
+        }
+    }
 }
